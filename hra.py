35d7c8debcc5080d5f6b310a7d9ad579dccba665
Suhajko <snircmarek10@gmail.com>
2024-06-26 17:55:09 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -7,8 +7,37 @@ win_height = 600
 window = display.set_mode((win_width, win_height))
 window.fill
 
-racket.png
-tenis_ball.png
+class GameSprite(sprite.Sprite):
+
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player.speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+        class Player(GameSprite):
+            def update_r(self):
+                keys = key.get_pressed()
+                if keys[K_UP] and self.rect.x > 5:
+                    self.rect.x -= self.speed
+                if keys[K_DOWN] and self.rect.x < win_width - 80:
+                    self.rect.x += self.speed
+            def update_l(self):
+                keys = key.get_pressed()
+                if keys[K_w] and self.rect.x > 5:
+                    self.rect.x -= self.speed
+                if keys[K_s] and self.rect.x < win_width - 80:
+                    self.rect.x += self.speed
+
+racket1 = Player("racket.png", 30, 200, 4, 50, 150)
+racket2 = Player("racket.png", 520, 200, 4, 50, 150)
+ball = GameSprite("tenis.png", 400, 200, 4, 50, 150)
+
 
 game = True
 while game:
