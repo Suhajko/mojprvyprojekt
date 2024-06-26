5d99498add793c0501a86b7df357e13a87826c54
Suhajko <snircmarek10@gmail.com>
2024-06-26 17:30:28 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -1,1 +1,20 @@
+from pygame import *
 
+
+back = (210, 255, 265)
+win_width = 700
+win_height = 600
+window = display.set_mode((win_width, win_height))
+window.fill
+
+racket.png
+tenis_ball.png
+
+game = True
+while game:
+    pass
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+display.update()
