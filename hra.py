2842463f6dd41d4caadf5cb7fa19217ac67c6a69
Suhajko <snircmarek10@gmail.com>
2024-10-02 17:34:18 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -2,7 +2,7 @@ from pygame import *
 
 class GameSprite(sprite.Sprite):
 
-    deff __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
  
         self.image = transform.scale(image.load(player_image), (wight, height)) 
